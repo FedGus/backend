e604297c5948d5553026c4e3ec0b2bb6389f4142
Olga Krasnova <krasolga2018@gmail.com>
2020-12-21 16:34:57 +0400

    добавление serve-static

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const history = require("connect-history-api-fallback");
 const app = express();
 const port = process.env.PORT || 8086;
 const serveStatic = require("serve-static");
+const path = require("path");
 
 // Парсинг json
 app.use(bodyParser.json());

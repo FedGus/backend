29434db220c1c5149510a19c818a448b31cb495f
Olga Krasnova <krasolga2018@gmail.com>
2020-12-21 23:03:49 +0400

    Обработка отдачи результата по апи для петиций

--- src/app/petition-list/petition-list.component.ts
+++ src/app/petition-list/petition-list.component.ts
@@ -1,27 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { CategorySortPipe } from '../shared/pipe/category-sort.pipe';
+import { Component, OnInit } from "@angular/core";
+import { CategorySortPipe } from "../shared/pipe/category-sort.pipe";
 import { MainService } from "../shared/services/main.service";
 
 @Component({
-  selector: 'app-petition-list',
-  templateUrl: './petition-list.component.html',
-  styleUrls: ['./petition-list.component.css']
+  selector: "app-petition-list",
+  templateUrl: "./petition-list.component.html",
+  styleUrls: ["./petition-list.component.css"],
 })
 export class PetitionListComponent implements OnInit {
-  public category = {every:true, remont:false, blago:false, ozelenenie:false, osvet:false};
+  public category = {
+    every: true,
+    remont: false,
+    blago: false,
+    ozelenenie: false,
+    osvet: false,
+  };
+  results: any;
   petitions: any;
-  constructor(private api: MainService) { }
+  constructor(private api: MainService) {}
 
   async ngOnInit() {
     try {
-      this.petitions = await this.api.get(
-        "/petitions"
-      );
-      console.log(this.petitions)
+      this.results = await this.api.get("/petitions");
+      this.petitions = this.results.petition;
+      console.log(this.petitions);
+    } catch (error) {
+      console.log(error);
     }
-    catch (error) {
-      console.log(error)
-    }
-
   }
 }
